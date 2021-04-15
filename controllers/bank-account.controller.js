8643264b5ea641c51dbb675ff369e85990f7f592
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-15 18:01:53 -0500

    Se genera las funciones para hacer consultas a la base de datos, al modelo de bancos

---
+++ controllers/bank-account.controller.js
@@ -0,0 +1,43 @@
+const { response, request } = require('express');
+const { BankAccount } = require('../models/BankAccount');
+
+const createAccount = async (req = request, res = response) => {
+    try {
+        const account = await BankAccount.create(req.body);
+        res.status(200).json({
+            ok: true,
+            account
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false
+        });
+    }
+}
+
+const queryAccountUser = async (req = request, res = response) => {
+    const { userId } = req.params;
+    try {
+        const accounts = await BankAccount.findAll({
+            where: {
+                userId
+            }
+        })
+
+        res.status(200).json({
+            ok: true,
+            accounts
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false
+        });
+    }
+}
+
+module.exports = {
+    createAccount,
+    queryAccountUser
+}
