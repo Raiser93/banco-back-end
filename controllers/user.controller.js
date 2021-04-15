742673f17ce73e884a9d0e38c5bebf865238048b
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-15 18:02:10 -0500

    Se genera las funciones para hacer consultas a la base de datos, al modelo de usuario

---
+++ controllers/user.controller.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcrypt');
+const { response, request } = require('express');
+const { generateJWT } = require('../helpers/jwt');
+const { User } = require('../models/User');
+
+// Registrar usuario
+const userRegister = async (req = request, res = response) => {
+    const {name, email, password} = req.body;
+    try {
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        const passwordBcrypt = bcrypt.hashSync(password, salt);
+
+        // Crear Usuario
+        const user = await User.create({name, email, password: passwordBcrypt});
+
+        // Generar JWT
+        const token = await generateJWT({
+            id: user.id,
+            name: user.name
+        });
+        
+        res.status(201).json({
+            ok: true,
+            id: user.id,
+            name: user.name,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: error.errors[0] ? error.errors[0].message : 'Ocurrio un error al tratar de registra el usuario',
+        })
+    }
+}
+
+module.exports = {
+    userRegister,
+}
