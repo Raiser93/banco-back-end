ace256b59b65984b258b0f7dfe5e16c31ea1c8cc
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-15 17:59:44 -0500

    Se añade las rutas, conexion a base de datos, modelos, body parser y cors al servidor cuando inicie

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser')
 const { dbConfig } = require('./config/database.conf');
-const { userRoutes } = require('./routes/user.routes');
-const {User} = require('./models/User');
 
+// Importar Rutas
+const { userRoutes } = require('./routes/user.routes');
+const { authRoutes } = require('./routes/auth.routes');
+const { bankAccountRoutes } = require('./routes/bank-account.routes');
+
+// Importar modelos de tablas
+const {User} = require('./models/User');
+const {BankAccount} = require('./models/BankAccount');
+
+// Crear Servidor Express
 const app = express();
 
-app.use('/api/user', userRoutes);
-
+// Base de datos
 dbConfig.sync().then(resp => {
     console.log('DB On');
 }).catch(err => {
     throw err
 });
+
+// Public
+app.use(express.static('public'));
+
+// CORS
+app.use(cors());
+
+// Lectura y parseo del body
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Rutas
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/bank', bankAccountRoutes);
+// app.use('/api/bank', userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('Server ON', process.env.PORT);
