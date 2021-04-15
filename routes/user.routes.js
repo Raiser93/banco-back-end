2516e679ae099d4e0819f55de336fc9b3ac9f7d5
Brayan Giraldo <bgiraldo020@gmail.com>
2021-04-15 18:06:15 -0500

    Se genera ruta para crear un usuario

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,25 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const userRoutes = Router();
 
-userRoutes.get('/:id', (req, res) => {
-    res.status(200).json({
-        ok: true,
-        id: req.params.id
-    })
-});
+// Controlador
+const {
+    userRegister,
+} = require('../controllers/user.controller');
+
+// Crear un usuario
+userRoutes.post(
+    '/register',
+    [ // Middlawers
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validateFields
+    ],
+    userRegister
+);
 
 module.exports = {
     userRoutes
