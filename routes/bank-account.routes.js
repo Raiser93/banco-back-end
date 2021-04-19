57a205708a3e015970befca8e399cf572532dbc7
Brayan <bgiraldo020@gmail.com>
2021-04-18 22:14:43 -0500

    Se genera rutas para crear y consultar las cuentas de terceros

--- routes/bank-account.routes.js
+++ routes/bank-account.routes.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { createAccount, queryAccountUser } = require('../controllers/bank-account.controller');
+const {
+    createAccount,
+    queryAccountUser,
+    createAccountThird,
+    getAccountThird
+} = require('../controllers/bank-account.controller');
 const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
@@ -9,6 +14,7 @@ const bankAccountRoutes = Router();
 bankAccountRoutes.post(
     '/create-account',
     [
+        validateJWT,
         check('num_account', 'El campo numero de cuenta es obligatorio').not().isEmpty(),
         check('type_account', 'El campo tipo de cuenta es obligatorio').not().isEmpty(),
         check('balance_account', 'El campo saldo es obligatorio').not().isEmpty(),
@@ -24,6 +30,29 @@ bankAccountRoutes.get(
         validateJWT
     ],
     queryAccountUser
+);
+
+bankAccountRoutes.get(
+    '/accounts-get-third/:userId',
+    [
+        validateJWT
+    ],
+    getAccountThird
+);
+
+bankAccountRoutes.post(
+    '/accounts-create-third/:userId',
+    [
+        validateJWT,
+        check('alias', 'El campo alias es obligatorio').not().isEmpty(),
+        check('bank', 'El entidad bancaria es obligatorio').not().isEmpty(),
+        check('type_account', 'El tipo de cuenta es obligatorio').not().isEmpty(),
+        check('num_account', 'El campo numero de cuenta es obligatorio').not().isEmpty(),
+        check('identification_number', 'El campo identificacion es obligatorio').not().isEmpty(),
+        check('type_currency', 'El campo tipo de moneda es obligatorio').not().isEmpty(),
+        validateFields
+    ],
+    createAccountThird
 );
 module.exports = {
     bankAccountRoutes
