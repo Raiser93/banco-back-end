eebd228bc0e50f04414ed19d5f1f206570e2be60
Brayan <bgiraldo020@gmail.com>
2021-04-18 22:00:54 -0500

    Se genera funcion para las cuenta de terceros

--- controllers/bank-account.controller.js
+++ controllers/bank-account.controller.js
@@ -1,5 +1,6 @@
 const { response, request } = require('express');
 const { BankAccount } = require('../models/BankAccount');
+const { BankAccountThird } = require('../models/BankAccountThird');
 
 const createAccount = async (req = request, res = response) => {
     try {
@@ -39,7 +40,84 @@ const queryAccountUser = async (req = request, res = response) => {
     }
 }
 
+const createAccountThird = async (req = request,res = response) => {
+    const { id, userId } = req.params;
+    const {
+        alias,
+        bank,
+        type_account,
+        num_account,
+        identification_number,
+        type_currency,
+    } = req.body;
+    try {
+        console.log({
+            alias,
+            bank,
+            type_account,
+            num_account,
+            identification_number,
+            type_currency,
+        });
+        const validateAccount = await BankAccountThird.findAll({
+            where: {
+                num_account,
+                userId
+            }
+        });
+
+        if (validateAccount.length) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El numero de cuenta ya esta registrado con el alias: ' + validateAccount[0].alias
+            });
+        }
+        const accountThrid = await BankAccountThird.create({
+            alias,
+            bank,
+            type_account,
+            num_account,
+            identification_number,
+            type_currency,
+            userId
+        });
+
+        res.status(200).json({
+            ok: true,
+            accountThrid
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: error.errors[0] ? error.errors[0].message : 'Ocurrio un error al tratar de crear una cuenta de terceros'
+        })
+    }
+}
+
+const getAccountThird = async (req = request, res = response) => {
+    const  { userId } = req.params;
+    try {
+        const accountsThird = await BankAccountThird.findAll({
+            where: {
+                userId
+            }
+        });
+
+        res.status(200).json({
+            ok: true,
+            accountsThird
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: error.errors[0] ? error.errors[0].message : 'Ocurrio un error al tratar de obtener las cuentas de terceros'
+        })
+    }
+}
+
 module.exports = {
     createAccount,
-    queryAccountUser
+    queryAccountUser,
+    createAccountThird,
+    getAccountThird
 }
